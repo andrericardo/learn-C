b41ca66e7c6fca38ecadb8efa0edb4d881ce6f4f
André Ricardo <andric87@gmail.com>
2024-10-07 19:29:43 +0100

    Using define.

--- fahrenheit-celsius.c
+++ fahrenheit-celsius.c
@@ -1,4 +1,8 @@
-# include <stdio.h>
+#include <stdio.h>
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
 
 /* print Fahrenheit-Celsius table
     for farh = 0, 20, ..., 300; floating-point version */
@@ -8,17 +12,12 @@ int main()
     printf("Fahr Celsius\n");
 
     float fahr, celsius;
-    int lower, upper, step;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
-    
-    fahr = lower;
-    while (fahr <= upper ) {
+    fahr = LOWER;
+    while (fahr <= UPPER ) {
         celsius = (5.0/9.0) * (fahr-32.0);
         printf("%3.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + step;
+        fahr = fahr + STEP;
     }
 
     return 0;
